119ebebf1866623ec996388923c758dd59096050
Lee Robinson <lrobinson2011@gmail.com>
2022-11-25 14:14:08 -0600

    usePathname

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,14 +1,15 @@
 'use client';
 
 import { Fragment } from 'react';
+import { usePathname } from 'next/navigation';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { signIn, signOut } from 'next-auth/react';
 import Image from 'next/image';
 
 const navigation = [
-  { name: 'Dashboard', href: '/', current: true },
-  { name: 'Playground', href: '/playground', current: false }
+  { name: 'Dashboard', href: '/' },
+  { name: 'Playground', href: '/playground' }
 ];
 
 function classNames(...classes: string[]) {
@@ -24,6 +25,8 @@ export function SignOut() {
 }
 
 export default function Navbar({ user }: { user: any }) {
+  const pathname = usePathname();
+
   return (
     <Disclosure as="nav" className="bg-white shadow-sm">
       {({ open }) => (
@@ -60,12 +63,12 @@ export default function Navbar({ user }: { user: any }) {
                       key={item.name}
                       href={item.href}
                       className={classNames(
-                        item.current
+                        pathname === item.href
                           ? 'border-slate-500 text-gray-900'
                           : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
                         'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
                       )}
-                      aria-current={item.current ? 'page' : undefined}
+                      aria-current={pathname === item.href ? 'page' : undefined}
                     >
                       {item.name}
                     </a>
@@ -150,12 +153,12 @@ export default function Navbar({ user }: { user: any }) {
                   as="a"
                   href={item.href}
                   className={classNames(
-                    item.current
+                    pathname === item.href
                       ? 'bg-slate-50 border-slate-500 text-slate-700'
                       : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800',
                     'block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
                   )}
-                  aria-current={item.current ? 'page' : undefined}
+                  aria-current={pathname === item.href ? 'page' : undefined}
                 >
                   {item.name}
                 </Disclosure.Button>
