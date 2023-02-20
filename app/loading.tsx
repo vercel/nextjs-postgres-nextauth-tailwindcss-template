d685f0de3d1c00a5e425fedc0d30b668ad11c074
Lee Robinson <lrobinson2011@gmail.com>
2023-02-20 13:49:45 -0600

    Improve search

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,5 @@
 import { Title, Text } from '@tremor/react';
+import Search from './search';
 
 export default async function Loading() {
   return (
@@ -7,6 +8,7 @@ export default async function Loading() {
       <Text>
         A list of users retrieved from a MySQL database (PlanetScale).
       </Text>
+      <Search disabled />
       <div className="tremor-base tr-relative tr-w-full tr-mx-auto tr-text-left tr-ring-1 tr-mt-6 tr-max-w-none tr-bg-white tr-shadow tr-border-blue-400 tr-ring-gray-200 tr-pl-6 tr-pr-6 tr-pt-6 tr-pb-6 tr-rounded-lg h-[360px]" />
     </main>
   );
--- app/search.tsx
+++ app/search.tsx
@@ -2,13 +2,11 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 import { usePathname, useRouter } from 'next/navigation';
-import { useState, useTransition } from 'react';
+import { useTransition } from 'react';
 
-export default function Search() {
-  const [focused, setFocused] = useState(false);
+export default function Search({ disabled }: { disabled?: boolean }) {
   const { replace } = useRouter();
   const pathname = usePathname();
-
   const [isPending, startTransition] = useTransition();
 
   function handleSearch(term: string) {
@@ -29,7 +27,7 @@ export default function Search() {
       <label htmlFor="search" className="sr-only">
         Search
       </label>
-      <div className="relative mt-1 rounded-md shadow-sm">
+      <div className="rounded-md shadow-sm">
         <div
           className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
           aria-hidden="true"
@@ -43,19 +41,18 @@ export default function Search() {
           type="text"
           name="search"
           id="search"
+          disabled={disabled}
           className="h-10 block w-full rounded-md border border-gray-200 pl-9 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
           placeholder="Search by name..."
           spellCheck={false}
-          onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
           onChange={(e) => handleSearch(e.target.value)}
         />
       </div>
 
-      {true && (
+      {isPending && (
         <div className="absolute right-0 top-0 bottom-0 flex items-center justify-center">
           <svg
-            className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+            className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-700"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
