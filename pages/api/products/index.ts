ad3f8836ab6e2db10ce016bc31ba7d20855222fb
Eduardo Parra <edue17@gmail.com>
2023-06-09 00:36:44 +0200

    fix

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,13 +1,16 @@
-import 'server-only';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from 'pages/api/auth/[...nextauth]';
 import { queryBuilder } from 'lib/planetscale';
 
-const addProduct = async (req: any, res: any) => {
-  const session = await getServerSession(req, res, authOptions);
-  console.log(req)
+export default async (req: any, res: any) => {
+  // only allow POST requests
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
 
-  if (req.method === 'POST' && session) {
+  const session = await getServerSession(req, res, authOptions);
+
+  if (session) {
     const { id, nombre, composicion, tipo, grupo, para, dosis, cuando, cultivo, ps, notas } = req.body;
     await queryBuilder
       .insertInto('productos')
@@ -25,15 +28,12 @@ const addProduct = async (req: any, res: any) => {
         notas
       ])
       .execute();
-    res.status(200).end();
+    res.status(201).end();
   } else {
     res.status(403).json({
       message:
         'You must be sign in to view the protected content on this page.'
     }).end();
   }
-
-  res.status(500).end();
-};
-
-export default addProduct;
+  
+}
