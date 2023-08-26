143267998ea1b41da5a1f81438109bd4caafa3cd
Malte Ubl <malte.ubl@gmail.com>
2023-08-26 12:46:58 -0700

    /home

---
+++ app/(actual-app)/layout.tsx
@@ -0,0 +1,31 @@
+import '../globals.css';
+
+import { Analytics } from '@vercel/analytics/react';
+import Nav from '../nav';
+import Toast from '../toast';
+import { Suspense } from 'react';
+
+export const metadata = {
+  title: 'Next.js 13 + PlanetScale + NextAuth + Tailwind CSS',
+  description:
+    'A user admin dashboard configured with Next.js, PlanetScale, NextAuth, Tailwind CSS, TypeScript, ESLint, and Prettier.'
+};
+
+export default async function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="h-full bg-gray-50">
+      <body className="h-full">
+        <Suspense>
+          <Nav />
+        </Suspense>
+        {children}
+        <Analytics />
+        <Toast />
+      </body>
+    </html>
+  );
+}
