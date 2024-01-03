11d9fdad1054a5d18e82b98d885d950645fe3a7d
tigger <tigger@cherground.com>
2024-01-03 17:16:55 +0900

    Layout - Navigator : 로고 변경

--- src/app/(DashboardLayout)/layout/shared/logo/Logo.tsx
+++ src/app/(DashboardLayout)/layout/shared/logo/Logo.tsx
@@ -1,20 +1,37 @@
-import Link from "next/link";
-import { styled } from "@mui/material";
-import Image from "next/image";
+import Link from 'next/link'
+import { styled } from '@mui/material'
+import Image from 'next/image'
 
 const LinkStyled = styled(Link)(() => ({
-  height: "40px",
-  width: "180px",
-  overflow: "hidden",
-  display: "block",
-}));
+  height: '40px',
+  width: '180px',
+  overflow: 'hidden',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  gap: '5px',
+}))
 
 const Logo = () => {
   return (
     <LinkStyled href="/">
-      <Image src="/images/logos/logo-dark.svg" alt="logo" height={40} width={105} priority />
+      <Image
+        src="/images/logos/simbol.svg"
+        alt="simbol"
+        height={30}
+        width={30}
+        priority
+      />
+      <Image
+        src="/images/logos/logo.svg"
+        alt="logo"
+        height={30}
+        width={135}
+        priority
+      />
     </LinkStyled>
-  );
-};
+  )
+}
 
-export default Logo;
+export default Logo
