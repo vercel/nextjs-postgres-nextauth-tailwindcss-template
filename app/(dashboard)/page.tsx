2a3ab99c5e103561dfe0d6e21c97ba5fc1bb941c
topus009 <sj_89@mail.ru>
2024-09-01 15:53:55 +0300

    fixes

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,7 +2,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { File, PlusCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { ProductsTable } from './products-table';
-import { getProducts } from '@/lib/db';
+// import { getProducts } from '@/lib/db';
 
 export default async function ProductsPage({
   searchParams
@@ -11,10 +11,10 @@ export default async function ProductsPage({
 }) {
   const search = searchParams.q ?? '';
   const offset = searchParams.offset ?? 0;
-  const { products, newOffset, totalProducts } = await getProducts(
-    search,
-    Number(offset)
-  );
+  // const { products, newOffset, totalProducts } = await getProducts(
+  //   search,
+  //   Number(offset)
+  // );
 
   return (
     <Tabs defaultValue="all">
@@ -43,11 +43,11 @@ export default async function ProductsPage({
         </div>
       </div>
       <TabsContent value="all">
-        <ProductsTable
+        {/* <ProductsTable
           products={products}
           offset={newOffset ?? 0}
           totalProducts={totalProducts}
-        />
+        /> */}
       </TabsContent>
     </Tabs>
   );
