7dd874cfa7ea2cc54be44a0f996f6c698711091f
Chris Tate <chris@ctate.dev>
2025-12-05 21:29:56 -0600

    Apply suggestion from @vercel[bot]
    Co-authored-by: vercel[bot] <35613825+vercel[bot]@users.noreply.github.com>

--- lib/db.ts
+++ lib/db.ts
@@ -14,9 +14,7 @@ import {
 import { count, eq, ilike } from 'drizzle-orm';
 import { createInsertSchema } from 'drizzle-zod';
 
-export const db = drizzle(
-  neon(process.env.POSTGRES_URL || 'postgresql://user:password@localhost:5432/dbname')
-);
+export const db = drizzle(neon(process.env.POSTGRES_URL!));
 
 export const statusEnum = pgEnum('status', ['active', 'inactive', 'archived']);
 
